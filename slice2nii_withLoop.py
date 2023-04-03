0df33451f93563c885205bfdc7794772317d3b50
Prabhleen Kaur <prabhleenkaur@berkeley.edu>
2023-04-03 11:36:02 -0700

    Adding slice2nii script that includes a loop to do many at once, from Yishu

---
+++ slice2nii_withLoop.py
@@ -0,0 +1,64 @@
+# convert a 2d histology/block tiff into a nift file
+
+import fnmatch
+import os
+from skimage import io
+import numpy as np
+import sys
+import glob
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.cm as cm
+from skimage import img_as_ubyte
+import skimage.color as color
+import nibabel as nib
+
+
+def save_nii(img,size,nii_file):
+    M = np.array([[size[0], 0, 0, 0],[0, size[1], 0, 0],[0, 0, size[2], 0],[0, 0, 0, 1]])
+    if img.ndim > 2:
+        if img.shape[2] == 3:
+            img = color.rgb2gray(img)
+        else:
+            img = img[...,0]
+    nii = nib.Nifti1Image(img, M)
+    nib.save(nii,nii_file)
+
+def create_nii(img,size):
+    M = np.array([[size[0], 0, 0, 0],[0, size[1], 0, 0],[0, 0, size[2], 0],[0, 0, 0, 1]])
+    #img = io.imread(img_file)
+    if img.ndim > 2:
+        if img.shape[2] == 3:
+            img = color.rgb2gray(img)
+        else:
+            img = img[...,0]
+    nii = nib.Nifti1Image(img, M)
+    return nii
+    #nib.save(nii,nii_file)
+
+def run_create_nii(img_file,x_size,y_size,z_size,nii_file):
+    size = np.array([x_size, y_size, z_size, 0])
+    img = io.imread(img_file)
+    nii = create_nii(img, size)
+    nib.save(nii,nii_file)
+
+def main():
+    if len(sys.argv) != 8:
+        print('Usage: slice2nii.py <tiff_path> <x_size> <y_size> <z_size> <nifti_path> <start> <end>')
+        exit()
+    img_file = str(sys.argv[1])  # abs path to where the images are
+    x_size = float(sys.argv[2])
+    y_size = float(sys.argv[3])
+    z_size = float(sys.argv[4])
+    nii_file = str(sys.argv[5])
+    start = int(sys.argv[6])
+    end = int(sys.argv[7])
+    for i in range(start, end + 1, 1):
+        file_name = img_file + str(i) + ".tif"
+        if os.path.isfile(file_name):
+            print(i)
+            nii_name = nii_file + str(i) + ".nii"
+            run_create_nii(file_name,x_size,y_size,z_size, nii_name)
+
+if __name__ == '__main__':
+    main()
