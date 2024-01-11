22b06e2658847e5e472f550e4b9b7caf3bfd2286
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-01-10 17:31:28 -0800

    Update stitch_images_wyn.sh with python2

--- wynton_image_files/stitch_images_wyn.sh
+++ wynton_image_files/stitch_images_wyn.sh
@@ -36,7 +36,7 @@ echo 'Export XML'
 #python /Users/prabhleenkaur/Box/Imaging_RO1_project/Wynton/LargeSlideScan/create_terastitch_xml.py $ROOT_DIR'/Metadata.txt' $RAW_DIR'/xml_import.xml' $RAW_DIR
 # Use Below version for when creating the image!
 #python /root/create_terastitch_xml.py $ROOT_DIR'/Metadata.txt' $RAW_DIR'/xml_import.xml' $RAW_DIR
-python /root/create_terastitch_xml.py $ROOT_DIR'/Metadata.txt' $RAW_DIR'/xml_import.xml' $RAW_DIR
+python2 /root/create_terastitch_xml.py $ROOT_DIR'/Metadata.txt' $RAW_DIR'/xml_import.xml' $RAW_DIR
 touch $ROOT_DIR'/expxml'
 
 echo 'Import'
