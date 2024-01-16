9e2c0b27102c2d450fb821a95a830c9425e54993
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-01-16 14:24:57 -0800

    Update run_pipeline_full.sh

--- scripts/run_pipeline_full.sh
+++ scripts/run_pipeline_full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-export PYTHONPATH=$PYTHONPATH:/home/LargeSlideScan/python/UCSFSlideScan
+export PYTHONPATH=$PYTHONPATH:/usr/local/bin
 
 if [ "$#" -ne 2 ]; then
 	echo "Usage: run_pipeline_full.sh <ROOT_DIR> <CONFIG_PATH>"
