c0f1f40bafbfc87abb55c0cce8cf88c2c9397b62
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-01-19 21:57:22 -0800

    Update run_pipeline_full.sh

--- scripts/run_pipeline_full.sh
+++ scripts/run_pipeline_full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-export PYTHONPATH=$PYTHONPATH:/usr/local/bin
+export PYTHONPATH=$PYTHONPATH: /usr/local/bin/high-res-3D-tau
 
 if [ "$#" -ne 2 ]; then
 	echo "Usage: run_pipeline_full.sh <ROOT_DIR> <CONFIG_PATH>"
