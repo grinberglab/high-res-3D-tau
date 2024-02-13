98de880d28c186f0073b33148025440b9f2dce53
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-02-12 20:28:10 -0800

    Add files via upload

---
+++ scripts/run_pipeline_full_python3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/bin/high-res-3D-tau
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: run_pipeline_full.sh <ROOT_DIR> <CONFIG_PATH>"
+	exit 0  
+fi
+
+ROOT_DIR=$1
+CONF_FILE=$2
+
+echo $ROOT_DIR
+echo $CONF_FILE
+
+python3 /usr/local/bin/high-res-3D-tau/pipeline/run_pipeline.py $ROOT_DIR $CONF_FILE
+
