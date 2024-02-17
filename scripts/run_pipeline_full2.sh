389994cf7b42c7fe12d2a18209c715099a2c8ac6
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-02-16 23:35:36 -0800

    Update run_pipeline_full2.sh

--- scripts/run_pipeline_full2.sh
+++ scripts/run_pipeline_full2.sh
@@ -13,5 +13,5 @@ CONF_FILE=$2
 echo $ROOT_DIR
 echo $CONF_FILE
 
-python2 /usr/local/bin/high-res-3D-tau/pipeline/run_pipeline_part2.py $ROOT_DIR $CONF_FILE
+python3 /usr/local/bin/high-res-3D-tau/pipeline/run_pipeline_part2.py $ROOT_DIR $CONF_FILE
 
