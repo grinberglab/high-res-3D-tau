59b557178a6fa0e0a89dae85d4064924be12fd7b
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-04-01 14:34:59 -0700

    Update run_network_segmentation.sh

--- scripts/run_network_segmentation.sh
+++ scripts/run_network_segmentation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 
+export PYTHONPATH=$PYTHONPATH:/usr/local/bin/high-res-3D-tau
+
 if [ "$#" -ne 2 ]; then
 	echo "Usage: run_network_segmentation.sh <ROOT_DIR> <CONFIG_PATH>"
 	exit 0  
@@ -11,4 +13,4 @@ CONF_FILE=$2
 echo $ROOT_DIR
 echo $CONF_FILE
 
-python /home/grinberg/Downloads/convnet_python3/network_segmentation.py $ROOT_DIR $CONF_FILE
+python3 /usr/local/bin/high-res-3D-tau/convnet_python3/network_segmentation.py $ROOT_DIR $CONF_FILE
