a89c0358eee979197d150f5a8e0333761c53b19a
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-04-03 14:26:11 -0700

    Update run_network_training.sh

--- scripts/run_network_training.sh
+++ scripts/run_network_training.sh
@@ -2,13 +2,15 @@
 
 export PYTHONPATH=$PYTHONPATH:/usr/local/bin/high-res-3D-tau
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: run_network_training.sh <CONFIG_PATH>"
+if [ "$#" -ne 2 ]; then
+	echo "Usage: run_network_training.sh <CONFIG_PATH> <LEARNING_RATE>"
 	exit 0  
 fi
 
 CONF_FILE=$1
+LEARNING_RATE=$2
 
 echo $CONF_FILE
+echo $LEARNING_RATE
 
-python3 /usr/local/bin/high-res-3D-tau/convnet_python3/network_segmentation.py train $CONF_FILE
+python3 /usr/local/bin/high-res-3D-tau/convnet_python3/network_trainer.py train $CONF_FILE $LEARNING_RATE
