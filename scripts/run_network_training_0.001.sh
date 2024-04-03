ded1ea4f438ad949605bae0857e9bb64705c7bb6
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-04-03 15:05:22 -0700

    Create run_network_training_0.001.sh

---
+++ scripts/run_network_training_0.001.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/bin/high-res-3D-tau
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: run_network_training_0.001.sh <CONFIG_PATH>"
+	exit 0  
+fi
+
+CONF_FILE=$1
+
+echo $CONF_FILE
+
+python3 /usr/local/bin/high-res-3D-tau/convnet_python3/network_trainer.py train $CONF_FILE 0.001
