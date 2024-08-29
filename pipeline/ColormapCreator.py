02f0ee66e5efec3c240a5afcd40ee37c45369879
JonathanLGUMI <90801826+JonathanLGUMI@users.noreply.github.com>
2024-08-29 13:15:07 -0700

    Update ColormapCreator.py

--- pipeline/ColormapCreator.py
+++ pipeline/ColormapCreator.py
@@ -52,10 +52,10 @@ class ColormapCreator(object):
         self.logger.addHandler(handler)
 
         #default values
-        #self.PIX_1MM = 2890  # 1mm= 2890 pixels for 10x RO1
-        #self.PIX_5MM = 14450  # 5mm = 14450 pixels for 10x RO1
-        self.PIX_1MM = 5814  # 1mm= 5814 pixels for 20x Luke Syn
-        self.PIX_5MM = 29070  # 5mm = 29070 pixels for 20x Luke Syn
+        self.PIX_1MM = 2890  # 1mm= 2890 pixels for 10x RO1
+        self.PIX_5MM = 14450  # 5mm = 14450 pixels for 10x RO1
+        #self.PIX_1MM = 5814  # 1mm= 5814 pixels for 20x Luke Syn
+        #self.PIX_5MM = 29070  # 5mm = 29070 pixels for 20x Luke Syn
         self.HMAP_RES = 0.1
         self.SCALE_FACTOR_VAL = 1000.0
 
